332ab8ac74f174c54d5c1cd1f2444a884be01bcd
Fusa-F <cist.b218.f.fukushi@gmail.com>
2021-04-05 18:43:48 +0900

    [update]コルーチンでカウントする処理を追加

--- Assets/Scripts/TimerComponent.cs
+++ Assets/Scripts/TimerComponent.cs
@@ -20,12 +20,35 @@ public class TimerComponent : MonoBehaviour
 
     private void Start()
     {
-        Observable.Interval(TimeSpan.FromSeconds(1))
-            .TakeWhile(_ => _timerReactiveProperty.Value > 0)
+        // TimeSpanでプロパティ更新する方法 ->TimeDisplayComponentでUIText表示
+        // Observable.Interval(TimeSpan.FromSeconds(1))
+        //     .TakeWhile(_ => _timerReactiveProperty.Value > 0)
+        //     .Subscribe(
+        //         _ => _timerReactiveProperty.Value--,
+        //         () => print("END")
+        //     )
+        //     .AddTo(gameObject); // ゲームオブジェクト破棄時に停止させる
+
+        // Coroutine内で発行する方法
+        Observable.FromCoroutine<int>(observer => CountDownCoroutine(observer, 5))
             .Subscribe(
-                _ => _timerReactiveProperty.Value--,
-                () => print("END")
-            )
-            .AddTo(gameObject); // ゲームオブジェクト破棄時に停止させる
+                count => print(count),
+                () => print("end")
+            );
+    }
+
+    private IEnumerator CountDownCoroutine(IObserver<int> observer, int startTime)
+    {
+        var currentTime = startTime;
+        while(currentTime > 0)
+        {
+            if(/*条件式=*/true)
+            {
+                observer.OnNext(currentTime--);
+            }
+            yield return new WaitForSeconds(1f);
+        }
+        observer.OnNext(0);
+        observer.OnCompleted();
     }
 }
